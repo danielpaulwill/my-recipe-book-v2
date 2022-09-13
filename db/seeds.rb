bdd8827dd3835f6d183586e9e03eb673527c6970
Daniel Williams <danielpaulwill@gmail.com>
2022-09-13 16:02:21 -0400

    seeds file built

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Seeding data..."
+
+5.times do
+  bakedGoods = Recipe.create(photo: Faker::LoremFlickr.image, title: Faker::Food.dish, description: Faker::Food.description, ingredients: [Faker::Food.ingredient, Faker::Food.ingredient, Faker::Food.ingredient, Faker::Food.ingredient, Faker::Food.ingredient], instructions: 'Bake it', category: 'Baked Goods')
+  drinks = Recipe.create(photo: Faker::LoremFlickr.image, title: Faker::Food.dish, description: Faker::Food.description, ingredients: [Faker::Food.ingredient, Faker::Food.ingredient, Faker::Food.ingredient, Faker::Food.ingredient, Faker::Food.ingredient], instructions: 'Pour it', category: 'Drinks')
+  entrees = Recipe.create(photo: Faker::LoremFlickr.image, title: Faker::Food.dish, description: Faker::Food.description, ingredients: [Faker::Food.ingredient, Faker::Food.ingredient, Faker::Food.ingredient, Faker::Food.ingredient, Faker::Food.ingredient], instructions: 'Cook it', category: 'Entrees')
+  sides = Recipe.create(photo: Faker::LoremFlickr.image, title: Faker::Food.dish, description: Faker::Food.description, ingredients: [Faker::Food.ingredient, Faker::Food.ingredient, Faker::Food.ingredient, Faker::Food.ingredient, Faker::Food.ingredient], instructions: 'Cook it', category: 'Sides')
+  # rand(1..5).times do
+  #   recipe.reviews.create(
+  #     title: Faker::Lorem.sentence, 
+  #     content: Faker::Markdown.sandwich(sentences: rand(3..6), repeat: rand(2..5)),
+  #     minutes_to_read: rand(3..50)
+  #   )
+  # end
+end
+
+
+puts "✅ Done seeding"
+
