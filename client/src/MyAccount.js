ec990a9a0bf93a85a5d061be54d3a06a82ad6c33
Daniel Williams <danielpaulwill@gmail.com>
2022-09-19 22:25:00 -0400

    code cleanup

--- client/src/MyAccount.js
+++ client/src/MyAccount.js
@@ -1,6 +1,5 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import Review from './Review';
 
 function MyAccount({ user }) {
 
--- client/src/Review.js
+++ client/src/Review.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 
 function Review({ review, id, reviewUser, user }) {
   const [canEdit, setCanEdit] = useState(false)
